f640210097cb67db72c7d6e574d77a49f619522a
daygarcia <day_garcia_anne@hotmail.com>
2024-04-27 04:13:35 -0300

    chore(docker): docker-compose complete

--- file-service/src/main.ts
+++ file-service/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://localhost:5672'],
+      urls: ['amqp://rabbitmq:5672'],
       queue: 'file_service_queue',
       queueOptions: {
         durable: false,
