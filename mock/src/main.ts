2e71c4fb09a8add387bb1aabddfbf65a3416d301
daygarcia <day_garcia_anne@hotmail.com>
2024-04-26 23:38:39 -0300

    feat: implement swagger

--- mock/src/main.ts
+++ mock/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mock API')
+    .setDescription('Mock data for testing purposes')
+    .setVersion('1.0')
+    .addTag('mock')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+  await app.listen(3002);
 }
 bootstrap();
