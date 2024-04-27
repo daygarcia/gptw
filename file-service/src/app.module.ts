eb1d58096827fca154a8b05d384497c5d7281f46
daygarcia <day_garcia_anne@hotmail.com>
2024-04-27 17:33:28 -0300

    chore: remove employees resource

--- file-service/src/app.module.ts
+++ file-service/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilesModule } from './files/files.module';
-import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [FilesModule, EmployeesModule],
+  imports: [FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
